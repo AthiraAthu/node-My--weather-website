6cc5d498ce9535c6f2102c28fc1bd63b69cb8ced
Athira <athirakundoor@gmail.com>
2020-08-31 12:39:55 +0530

    Final modification

--- src/utils/weatherApp.js
+++ src/utils/weatherApp.js
@@ -5,7 +5,6 @@ const findWeather= (address,callback)=>{
     const url=`http://api.openweathermap.org/data/2.5/weather?q=${address}&units=metric&appid=8770e174965014c5f824dad3ef2acc21`
 
     request(url,(error, response, body) => {
-        let res='';
         const data=JSON.parse(body)
         if(data.cod==='404'){
             callback(data.cod)
